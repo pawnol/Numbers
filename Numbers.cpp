b0e7a7464c0e8afa6a7f58c2b9c2bf184b03aa85
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-03-14 14:41:30 -0500

    Add files via upload

---
+++ Numbers.cpp
@@ -0,0 +1,17 @@
+/*
+Numbers
+3/14/2021
+The program currently displays the number 16. Change the code so that the
+program displays the number 64.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int digit1 = 1;
+    int digit2 = 6;
+    cout << digit1 << digit2 << "\n";
+    return 0;
+}
